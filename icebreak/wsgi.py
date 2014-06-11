4f38daa9129feb1ede89c43d30f699248b7b89de
Kapor <smokingkapor@gmail.com>
2014-06-11 16:32:55 +0800

    Start django project

---
+++ icebreak/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for icebreak project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icebreak.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
